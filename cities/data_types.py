1408e3e425322e7dcba2f54c7d92102dd631408b
JoeMilius <joemilius@gmail.com>
2022-06-09 21:50:08 -0600

    list and print practice python

--- cities/data_types.py
+++ cities/data_types.py
@@ -5,3 +5,9 @@ item_4 = (7 == 4)
 item_5 = (4 >= 7)
 
 print(type(item_1))
+
+top_travel_cities = ['Solta', 'Greenville', 'Buenos Aires', 'Los Cabos', 'Walla Walla Valley', 'Marakesh', 'Albuquerque', 'Archipelago Sea', 'Iguazu Falls', 'Salina Island', 'Toronto', 'Pyeongchang']
+top_canadian_city = top_travel_cities[-2]
+print(top_travel_cities)
+print(top_travel_cities[-1])
+print(top_canadian_city)
